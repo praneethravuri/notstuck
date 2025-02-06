21d6ff75f2bfe554f0d9b0ee0b652ba9875b67cb
Praneeth Ravuri <pravdevrav@gmail.com>
2025-02-05 22:29:10 -0600

    changed embedding function

--- backend/rag/get_embedding_function.py
+++ backend/rag/get_embedding_function.py
@@ -11,4 +11,4 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 def get_embedding_function():
-    return OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    return OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY, model="text-embedding-3-small")
